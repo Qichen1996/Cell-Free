807639bb8e2897cd3732f0b1ca0f150ec5bb5bb8
Qichen1996 <qichenw@kth.se>
2025-04-19 23:35:50 +0200

    initial update

---
+++ con.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pip install icecream
+pip install gym
+pip install gymnasium
+pip install plotly
+pip install wandb
+pip install -U kaleido
+pip install pandas==1.3.5
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
